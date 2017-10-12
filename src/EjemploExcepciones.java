672dc491ccef62747a7d9dbfb0b1a23e15dee6c3
omarnavarro0012 <omm27@hotmail.com>
2017-10-12 10:45:29 -0500

    Initial commit

---
+++ src/EjemploExcepciones.java
@@ -0,0 +1,11 @@
+
+/**
+ * Created by Alumnos on 12/10/2017.
+ */
+public class EjemploExcepciones
+{
+    public static void main(String[] args)
+    {
+        System.out.print("hola!");
+    }
+}
